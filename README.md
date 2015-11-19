a7af4cd55ce8f903ed67754ba5b18c9574be854b
liviu <liviu.teris@gmail.com>
2015-11-19 14:55:44 +0100

    SYN HS, Keep-alive

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 
 python3 -m tcpsim.TCPSim "10.204.70.34" 102 eth0 1
+
+ sudo iptables -A OUTPUT -d 10.204.70.34 -p tcp --tcp-flags RST RST -j DROP
--- tcpsim/TCPSession.py
+++ tcpsim/TCPSession.py
@@ -14,6 +14,7 @@ import time
 import functools
 import ctypes
 import fcntl
+import os
 
 flagDict = {"F":0x1, "S":0x2, "R":0x4, "P":0x8, 
             "A":0x10, "U":0x20, "E":0x40, "C":0x80}
@@ -21,31 +22,6 @@ flagDict = {"F":0x1, "S":0x2, "R":0x4, "P":0x8,
 class ifreq(ctypes.Structure):
     _fields_ = [("ifr_ifrn", ctypes.c_char * 16),
                 ("ifr_flags", ctypes.c_short)]
-
-def setSockPromisc(sock, iface):
-    IFF_PROMISC = 0x100
-    SIOCGIFFLAGS = 0x8913
-    SIOCSIFFLAGS = 0x8914
-    
-    ifr = ifreq()
-    ifr.ifr_ifrn = byte(iface)
-    
-    fcntl.ioctl(sock.fileno(), SIOCGIFFLAGS, ifr)
-    ifr.ifr_flags |= IFF_PROMISC    
-    fcntl.ioctl(sock.fileno(), SIOCSIFFLAGS, ifr)
-    
-def unsetSockPromisc(sock, iface):
-    IFF_PROMISC = 0x100
-    SIOCGIFFLAGS = 0x8913
-    SIOCSIFFLAGS = 0x8914
-    
-    ifr = ifreq()
-    ifr.ifr_ifrn = byte(iface)
-    
-    fcntl.ioctl(sock.fileno(), SIOCGIFFLAGS, ifr)
-    ifr.ifr_flags &= ~IFF_PROMISC
-    fcntl.ioctl(sock.fileno(), SIOCSIFFLAGS, ifr)
-
 
 def getLocalIp(iface = 'eth0'):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -63,8 +39,6 @@ def getLocalIp(iface = 'eth0'):
     sock.close()
     return ip
   
-
-
 def getSrcIp(bstr):
     return "".join(str(int(i)) + '.' for i in bstr[12:16])[:-1]
 
@@ -86,6 +60,9 @@ def printPkt(bstr):
     print(getSrcIp(bstr)+"|" + getDstIp(bstr) + "|" 
           + str(getSrcPort(bstr)) +"|" + str(getDstPort(bstr)))
     print([hex(i) for i in bstr])
+
+def getFlagsFromStr(flags):
+    return functools.reduce(lambda x, y: x | flagDict[y], flags, 0)
 
 '''
     flags C E U A P R S F
@@ -122,44 +99,39 @@ class TCPSession(Thread):
         self._iface = iface
         
         #get a port and bind to it
-        try:
-            
+        try:            
             # create a raw socket and bind it to the public interface
             self._sockout = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.SOCK_RAW)
             self._sockout.setsockopt(socket.IPPROTO_IP, socket.SO_REUSEADDR, 1)
             HOST = socket.gethostbyname(socket.gethostname())
             
             self._sockout.bind((self._localIp, 0))
-            self._localPort = random.randint(10000,60000)
+            self._localPort = random.randint(33000,60000)            
             self._sockout.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-                                   
-            
-            
+
             self._sockin = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)            
         except socket.error as e:
             print(">>>>> Error building socket" + str(e))
             
         self._state = "STATE_SYN"
+        os.system("iptables -A OUTPUT -d " + self._serverIp + 
+                  " -p tcp --tcp-flags RST RST -j DROP")
 
     def shutdown(self):
         try:
             self._sockin.close()
             self._sockout.close()
+            os.system("iptables -D OUTPUT -d " +
+                      self._serverIp + " -p tcp --tcp-flags RST RST -j DROP")
         except socket.error as e:
             print(e)
         
-    def run(self):
+    def run(self):        
         while self._state != "STATE_DONE":
             fct = self.fsm[self._state]["act"]
             fct()
             #self._state = fsm[self._state]["goto"]
-            break #remove
-            
-        (bstr, _) = self._sockin.recvfrom(65565)
-        if self.inPktMatches(bstr):
-            ip = disIpPacket(bstr)
-            ip.show()
-            printPkt(bstr)
+            #break #remove
             
                 
     def tstSelf(self):
@@ -173,27 +145,69 @@ class TCPSession(Thread):
     def inPktMatches(self, bstr):
         return (pktIsTCP(bstr) and 
                 getSrcIp(bstr) == self._serverIp and
-                getDstIp(bstr) == self._localIp and  
+                getDstIp(bstr) == self._localIp and
                 getSrcPort(bstr) == self._serverPort and 
                 getDstPort(bstr) == self._localPort)
         
     ''' initiate and complete syn hs '''    
     def doSynHS(self):
-        seqNo = random.randint(1000,60000)
+        self._seqNo = random.randint(1000,60000)
+        self._ackNo = self._seqNo + 1
         bpkt = genIpPacket(self._localIp, self._serverIp, 
-                           self._localPort, self._serverPort, seqNo, 0, "S")
+                           self._localPort, self._serverPort, self._seqNo, 0, "S")
         
+        #send SYN
         self._sockout.sendto(bpkt, (self._serverIp, 0))
+        
+        #get SYN-ACK
+        while 1:
+            (bstr, _) = self._sockin.recvfrom(65565)
+            if self.inPktMatches(bstr):
+                ip = disIpPacket(bstr)                
+                if (ip.payload.payload.getfieldval("flags") != 
+                    getFlagsFromStr("SA") or 
+                    ip.payload.payload.getfieldval("ack") != self._ackNo):
+                    self._state = "STATE_DONE"
+                    return
+                self._ackNo = ip.payload.payload.getfieldval("seq") + 1
+                break
+        
+        #send SYN
+        self._seqNo += 1
+        bpkt = genIpPacket(self._localIp, self._serverIp, 
+                           self._localPort, self._serverPort, self._seqNo, self._ackNo, "A")
+        self._sockout.sendto(bpkt, (self._serverIp, 0))            
         
         self._state = "STATE_KA"
     
     def doKA(self):
-        print(">>>>>>>>>>> doKA")                
+         #get ACK (KA)
+        while 1:
+            (bstr, _) = self._sockin.recvfrom(65565)
+            if self.inPktMatches(bstr):
+                ip = disIpPacket(bstr)
+                flgs = ip.payload.payload.getfieldval("flags")                
+                if  flgs == getFlagsFromStr("A"):
+                    self._ackNo = ip.payload.payload.getfieldval("seq") + 1
+                    self._seqNo = ip.payload.payload.getfieldval("ack")
+                    break
+                elif flgs == getFlagsFromStr("FA") or flgs == getFlagsFromStr("F"):
+                    self._ackNo = ip.payload.payload.getfieldval("seq") + 1
+                    self._seqNo = ip.payload.payload.getfieldval("ack")
+                    self._state = "STATE_FIN" 
+                    return
+                else:
+                    self._state = "STATE_DONE"
+                    return                                
+        
+        bpkt = genIpPacket(self._localIp, self._serverIp, 
+                           self._localPort, self._serverPort, self._seqNo, self._ackNo, "A")
+        self._sockout.sendto(bpkt, (self._serverIp, 0))    
+        self._state = "STATE_KA"
     
     def doFin(self):
         print(">>>>>>>>>>> doFIN")
-        while 1:
-            pass
+        self._state = "STATE_DONE"
     
 class BrokenTCP(TCPSession):
     
